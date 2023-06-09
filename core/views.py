cb39f6ad8c16579e108a8f59d6843a3677e6da1d
malinapavel <malina.pavel@student.upt.ro>
2023-06-09 15:40:13 +0300

    New entries page designed

--- core/views.py
+++ core/views.py
@@ -11,6 +11,8 @@ import urllib
 
 
 url_list = []
+green_list = []
+black_list = []
 
 config = {
 	'apiKey': "AIzaSyC-DF0lyMXYegJlpKiUrQFzFOMn6QtI_lo",
@@ -22,11 +24,24 @@ config = {
 cred = firebase_admin.credentials.Certificate('key.json')
 default_app = firebase_admin.initialize_app(cred, config)
 bucket = storage.bucket()
-bucket_list = bucket.list_blobs(prefix="DEFAULT/")
-for blob in bucket_list:
+
+default_bucket_list = bucket.list_blobs(prefix="DEFAULT/")
+for blob in default_bucket_list:
     img_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
     url_list.append(img_url)
 
-def index(request):
+# green_bucket_list = bucket.list_blobs(prefix="GREENLIST/")
+# for blob in green_bucket_list:
+#     img_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+#     green_list.append(img_url)
 
+# black_bucket_list = bucket.list_blobs(prefix="BLACKLIST/")
+# for blob in black_bucket_list:
+#     img_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+#     black_list.append(img_url)
+
+def index(request):
     return render(request, 'index.html', {'images_url': url_list[1:6]})
+
+def new_entries(request):
+    return render(request, 'new-entries.html', {'images_url': url_list[1:]})
