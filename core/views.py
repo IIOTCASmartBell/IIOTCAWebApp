be9b473928818af83135170d79f590ea65cc8f49
malinapavel <malina.pavel@student.upt.ro>
2023-06-10 11:53:37 +0300

    Can modify the name of the person from a specific image in New Entries

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from collections import abc
 collections.MutableMapping = abc.MutableMapping
 import firebase_admin
 from firebase_admin import storage
+from firebase_admin import db
 import datetime
 import requests
 import urllib
@@ -13,17 +14,30 @@ import urllib
 url_list = []
 green_list = []
 black_list = []
+display_new_entries = {}
+
+# config = {
+# 	'apiKey': "AIzaSyC-DF0lyMXYegJlpKiUrQFzFOMn6QtI_lo",
+# 	'authDomain': "smartdoorbell-2382f.firebaseapp.com",
+# 	'databaseURL': "https://smartdoorbell-2382f-default-rtdb-firebaseio.com",
+#   'storageBucket': "smartdoorbell-2382f.appspot.com"
+# }
+
 
 config = {
-	'apiKey': "AIzaSyC-DF0lyMXYegJlpKiUrQFzFOMn6QtI_lo",
-	'authDomain': "smartdoorbell-2382f.firebaseapp.com",
-	'databaseURL': "https://smartdoorbell-2382f-default-rtdb-firebaseio.com",
-  'storageBucket': "smartdoorbell-2382f.appspot.com"
+	'apiKey': "AIzaSyASpod8P7JnGBYLDz6JO8Q2C3Dbss9YmBw",
+	'authDomain': "smartdoorbellfinal.firebaseapp.com",
+	'databaseURL': "https://smartdoorbellfinal-default-rtdb.firebaseio.com",
+	'storageBucket': "smartdoorbellfinal.appspot.com"
 }
 
-cred = firebase_admin.credentials.Certificate('key.json')
+# cred = firebase_admin.credentials.Certificate('key.json')
+cred = firebase_admin.credentials.Certificate('key2.json')
 default_app = firebase_admin.initialize_app(cred, config)
 bucket = storage.bucket()
+ref_act = db.reference('/actions')
+ref_lists = db.reference('/lists')
+ref_default = db.reference('/lists/default').get()
 
 default_bucket_list = bucket.list_blobs(prefix="DEFAULT/")
 for blob in default_bucket_list:
@@ -41,7 +55,24 @@ for blob in default_bucket_list:
 #     black_list.append(img_url)
 
 def index(request):
-    return render(request, 'index.html', {'images_url': url_list[1:6]})
+    return render(request, 'index.html', {'images_url': url_list[:5]})
 
 def new_entries(request):
-    return render(request, 'new-entries.html', {'images_url': url_list[1:]})
+    for key, value in ref_default.items():
+        for keyval, val in value.items():
+            display_new_entries[key] = val
+            break
+
+    if request.method == 'POST':
+        if 'image_key' in request.POST:
+            key_img = request.POST['image_key']
+            if 'name_person' in request.POST and 'label_person' in request.POST:
+                name = request.POST['name_person']
+                label = request.POST['label_person']
+
+                ref_image = db.reference('/lists/default/' + key_img)
+                ref_image.update({
+                    'person_name': name
+                })
+
+    return render(request, 'new-entries.html', {'images_url': display_new_entries})
